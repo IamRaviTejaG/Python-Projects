7f7f0107c28cf800c562d23a16327f16e5097790
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-01-26 12:09:00 +0530

    Update IIIT-Bh Rosei.py

--- IIIT-Bh Rosei.py
+++ IIIT-Bh Rosei.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-browser.get = ("https://172.16.2.200:8081/rosei/login.jsp")
+browser.get = ("https://172.16.2.200:8081/rosei/login.jsp") #The Rosei URL.
 print ("Welcome to Rosei Automation Tool\n\n")
-x = raw_input("Input R1 for Roseighara-1 and R2 for Roseighara-2: ")
-username = "B2160XX" #Edit the username and password fields before proceeding.
-paword = "password"
 
+"""Inputs section. Input your username, password and preferred Roseighara for once."""
+x = raw_input ("Input 'R1' for Roseighara-1 and 'R2' for Roseighara-2: ")
+username = raw_input ("Enter your username: ")
+paword = raw_input ("Enter your password: ")
 
+"""Browser action starts here. Before editing, please refer the Selenium Documentation."""
 uname = browser.find_element_by_name('un')
 pword = browser.find_element_by_name('pw')
 uname.send_keys(username)
